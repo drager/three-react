74378d3b9afd116bd3e24b6e931665aa96036d2d
Jesper Håkansson <jesper@jesperh.se>
2021-05-04 19:08:22 +0200

    Add three js and switch textures

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,140 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import * as THREE from 'three'
+const OrbitControls = require('three-orbit-controls')(THREE)
 
-function App() {
+const textureArray = [
+  'https://s3-us-west-2.amazonaws.com/s.cdpn.io/259155/THREE_gates.jpg',
+  'https://s3-us-west-2.amazonaws.com/s.cdpn.io/259155/THREE_crate1.jpg',
+  'https://s3-us-west-2.amazonaws.com/s.cdpn.io/259155/THREE_crate2.jpg',
+]
+
+const Canvas = React.memo(
+  () => {
+    const ref = React.useRef(null)
+
+    const geometry = new THREE.BoxGeometry(1, 1, 1)
+    const material = new THREE.MeshBasicMaterial()
+
+    // instantiate a texture loader
+    const loader = new THREE.TextureLoader()
+
+    const cube = new THREE.Mesh(geometry, material)
+
+    const scene = new THREE.Scene()
+    scene.background = new THREE.Color(0xcccccc)
+
+    const camera = new THREE.PerspectiveCamera(
+      75,
+      window.innerWidth / window.innerHeight,
+      0.1,
+      1000
+    )
+    camera.position.z = 5
+
+    const renderer = new THREE.WebGLRenderer()
+    renderer.setSize(window.innerWidth, window.innerHeight)
+    renderer.domElement.id = 'canvas' // set ID on 3D canvas
+
+    //allow cross origin loading
+    loader.crossOrigin = ''
+
+    const controls = new OrbitControls(camera, renderer.domElement)
+    controls.update()
+
+    React.useEffect(() => {
+      if (ref.current) {
+        // @ts-ignore
+        ref.current.appendChild(renderer.domElement)
+
+        const animate = () => {
+          requestAnimationFrame(animate)
+          renderer.render(scene, camera)
+        }
+        animate()
+      }
+    }, [])
+
+    return (
+      <>
+        <div id="threed" ref={ref} />
+        <ChangeTexture
+          loader={loader}
+          material={material}
+          scene={scene}
+          cube={cube}
+        />
+      </>
+    )
+  },
+  // Only render once. We never want to rerender this part!
+  () => true
+)
+
+const buttonStyle = {
+  position: 'absolute',
+  top: 50,
+  left: 50,
+  backgroundColor: 'purple',
+  padding: 16,
+  border: 'none',
+  color: 'white',
+  borderRadius: 5,
+} as any
+
+const ChangeTexture = ({ loader, material, scene, cube }: any) => {
+  const { texture, setTexture } = React.useContext(GlobalStateContext)
+
+  const update = React.useCallback(() => {
+    if (texture > textureArray.length - 1) {
+      setTexture(0)
+    } else {
+      setTexture(texture + 1)
+    }
+  }, [texture])
+
+  React.useEffect(() => {
+    // @ts-ignore
+    loader.load(textureArray[texture], function (tex: any) {
+      // Once the texture has loaded assign it to the material
+      material.map = tex
+
+      // Add the mesh into the scene
+      scene.add(cube)
+    })
+  }, [texture])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <button id="cube-btn" style={buttonStyle} onClick={update}>
+      Change texture {texture}
+    </button>
+  )
 }
 
-export default App;
+const GlobalStateContext = React.createContext({
+  texture: 0,
+  setTexture: (texture: number) => {},
+})
+
+function App() {
+  const [texture, setTexture] = React.useState(0)
+
+  const update = React.useCallback((updatedTexture) => {
+    setTexture(updatedTexture)
+    notifyOthers(updatedTexture)
+  }, [])
+
+  const defaultValue = { texture, setTexture: update }
+
+  const notifyOthers = React.useCallback((updatedTexture) => {
+    // Notify others...
+    console.log('updatedTexture', updatedTexture)
+  }, [])
+
+  return (
+    <GlobalStateContext.Provider value={defaultValue}>
+      <Canvas />
+    </GlobalStateContext.Provider>
+  )
+}
+
+export default App
